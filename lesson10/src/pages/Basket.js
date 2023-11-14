515cdc07907a862e1fceaab7435c771fe0ab8057
Elmurod Vokhidov <101036617+elmurodvokhidov@users.noreply.github.com>
2023-11-14 22:34:43 +0500

    Delete lesson10 directory

--- lesson10/src/pages/Basket.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Basket() {
-  return (
-    <div>Basket</div>
-  )
-}
-
-export default Basket
--- lesson10/src/pages/Favorite.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Favorite() {
-  return (
-    <div>Favorite</div>
-  )
-}
-
-export default Favorite
